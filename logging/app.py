922047a4f8c600df044abcd722da70fb04d7a380
Bhavya Phogat <b.phogat1925@gmail.com>
2023-07-05 23:42:25 -0400

    log motion history to a database and view data from last 24 hrs

---
+++ logging/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, Response, request
+import sqlite3
+from motion_utils import generate_sums
+from threading import Thread
+from time import sleep, time
+
+
+# constants
+NUM_DATA_CHUNKS = 24
+DB_NAME = 'bird.sqlite3'
+
+
+# sqlite setup
+conn = sqlite3.connect(DB_NAME)
+with conn:
+    conn.execute('CREATE TABLE IF NOT EXISTS motion_logs ( time FLOAT, motion FLOAT )')
+
+
+# define threads
+
+# continuously update display with motion data
+def display_motion_data():
+    while 1:
+        # get data
+        motion_data = generate_sums(NUM_DATA_CHUNKS, sqlite3.connect(DB_NAME))
+        print(motion_data)
+        # TODO draw graph on display
+        # delay
+        sleep(100)
+
+# continuously update time on display
+def update_time():
+    while 1:
+        # TODO draw time on display
+        sleep(1)
+
+# start threads
+Thread(target=display_motion_data).start()
+Thread(target=update_time).start()
+
+
+# flask
+app = Flask(__name__)
+
+@app.route('/log', methods=['POST'])
+def log():
+    # retrieve data
+    motion_factor = float(request.data)
+    
+    # scale up smaller values
+    # motion_factor = motion_factor ** 0.25
+
+    # insert into db
+    conn.execute('INSERT INTO motion_logs (time, motion) VALUES (?, ?)', (time(), motion_factor))
+
+    print('motion_factor', motion_factor)
+    return Response(status=200)
+
+if __name__ == '__main__':
+    app.run(port=3001)
---
+++ logging/motion_utils.py
@@ -0,0 +1,53 @@
+from perlin_noise import PerlinNoise
+from time import time
+from datetime import timedelta
+import numpy as np
+import sqlite3
+from random import random
+
+
+# generate 24 hours of data, 2 points per second
+def generate_test_data():
+    conn = sqlite3.connect('test.sqlite3')
+    with conn:
+        conn.execute('DROP TABLE IF EXISTS motion_logs')
+        conn.execute('CREATE TABLE motion_logs ( time FLOAT, motion FLOAT )')
+
+    noise = PerlinNoise(seed=time(), octaves=3)
+    day_length = int(2 * timedelta(days=1).total_seconds())
+    with conn:
+        now = time()
+        t = time() - timedelta(days=1).total_seconds()
+        for i in range(day_length):
+            if random() < 0.95:
+                n = max(0, noise(i/day_length))
+                conn.execute('INSERT INTO motion_logs (time, motion) VALUES (?, ?)', (t, n))
+            t += 0.5
+            if t > now:
+                break
+
+# generate sums for each interval over the last 24 hours
+def generate_sums(num_chunks: int, conn: sqlite3.Connection):
+    interval = timedelta(days=1/num_chunks).total_seconds()
+    num_logs_expected = interval * 2 # there are expected to be 2 logs per second (max)
+    now = time()
+    t1 = now - timedelta(days=1).total_seconds()
+    motion_data = []
+    while t1 < now:
+        # get sum of all logs in the given time interval
+        with conn:
+            res = conn.execute('SELECT COUNT(motion), SUM(motion) FROM motion_logs WHERE time >= ? AND time < ?', (t1, t1+interval))
+        num_logs, motion_sum = res.fetchall()[0]
+        t1 += interval
+        # if no logs found for this time interval, sum is 0
+        if not num_logs:
+            motion_data.append(0)
+            continue
+        # calculate weighted sum to account for potential missing logs
+        motion_sum = num_logs_expected / num_logs * motion_sum
+        motion_data.append(motion_sum)
+    return motion_data
+
+if __name__ == '__main__':
+    generate_test_data()
+    print( generate_sums(num_chunks=24, conn=sqlite3.connect('test.sqlite3')) )
