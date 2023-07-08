494eeb1870fb8aa3c2b9429f3c5ba57321c9f1ea
Bhavya Phogat <b.phogat1925@gmail.com>
2023-07-08 17:04:04 -0400

    display motion data on external display

--- logging/app.py
+++ logging/app.py
@@ -1,51 +1,19 @@
 from flask import Flask, Response, request
-import sqlite3
-from motion_utils import generate_sums
+from motion_utils import get_db_connection, get_motion_data, NUM_DATA_CHUNKS
 from threading import Thread
-from time import sleep, time
+from time import time
+from update_display import display_motion_data
 
-
-# constants
-NUM_DATA_CHUNKS = 24
-DB_NAME = 'bird.sqlite3'
-
-
-# sqlite setup
-def get_db_connection():
-    return sqlite3.connect(DB_NAME)
-
-with get_db_connection() as conn:
-    conn.execute('CREATE TABLE IF NOT EXISTS motion_logs ( time FLOAT, motion FLOAT )')
-
-
-# define threads
 
 # continuously update display with motion data
-def display_motion_data():
-    while 1:
-        # get data
-        motion_data = generate_sums(NUM_DATA_CHUNKS, get_db_connection())
-        # TODO draw graph on display
-        # delay
-        sleep(10)
-
-# continuously update time on display
-def update_time():
-    while 1:
-        # TODO draw time on display
-        sleep(1)
-
-# start threads
 Thread(target=display_motion_data).start()
-Thread(target=update_time).start()
-
 
 # flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = generate_sums(NUM_DATA_CHUNKS, get_db_connection())
+    data = get_motion_data(NUM_DATA_CHUNKS, get_db_connection())
     res = [f'{str(i+1).zfill(2)}: {sum}<br>' for i,sum in enumerate(data)]
     return Response(res)
 
@@ -54,14 +22,10 @@ def log():
     # retrieve data
     motion_factor = float(request.data)
     
-    # scale up smaller values
-    # motion_factor = motion_factor ** 0.25
-
     # insert into db
     with get_db_connection() as conn:
-        res = conn.execute('INSERT INTO motion_logs (time, motion) VALUES (?, ?)', (time(), motion_factor))
+        conn.execute('INSERT INTO motion_logs (time, motion) VALUES (?, ?)', (time(), motion_factor))
 
-    print('motion_factor', motion_factor)
     return Response(status=200)
 
 if __name__ == '__main__':
--- logging/motion_utils.py
+++ logging/motion_utils.py
@@ -1,10 +1,19 @@
 from perlin_noise import PerlinNoise
 from time import time
 from datetime import timedelta
-import numpy as np
 import sqlite3
 from random import random
 
+
+# constants
+NUM_DATA_CHUNKS = 24*2
+DB_NAME = 'bird.sqlite3'
+
+# sqlite setup
+def get_db_connection():
+    return sqlite3.connect(DB_NAME)
+with get_db_connection() as conn:
+    conn.execute('CREATE TABLE IF NOT EXISTS motion_logs ( time FLOAT, motion FLOAT )')
 
 # generate 24 hours of data, 2 points per second
 def generate_test_data():
@@ -27,31 +36,46 @@ def generate_test_data():
                 break
 
 # generate sums for each interval over the last 24 hours
-def generate_sums(num_chunks: int, conn: sqlite3.Connection):
+def get_motion_data(num_chunks: int, conn: sqlite3.Connection):
     interval = timedelta(days=1/num_chunks).total_seconds()
     num_logs_expected = interval * 2 # there are expected to be 2 logs per second (max)
     now = time()
     t1 = now - timedelta(days=1).total_seconds()
+    
     # remove old logs
     with conn:
         conn.execute('DELETE FROM motion_logs WHERE time < ?', [t1])
-    # gather motion data
+        
+    # gather 24hr motion data
     motion_data = []
     while t1 < now:
         # get sum of all logs in the given time interval
         with conn:
             res = conn.execute('SELECT COUNT(motion), SUM(motion) FROM motion_logs WHERE time >= ? AND time < ?', (t1, t1+interval))
-        num_logs, motion_sum = res.fetchall()[0]
+        num_logs, motion_sum = res.fetchone()
         t1 += interval
+        
         # if no logs found for this time interval, sum is 0
         if not num_logs:
             motion_data.append(0)
             continue
+        
         # calculate weighted sum to account for potential missing logs
         motion_sum = num_logs_expected / num_logs * motion_sum
         motion_data.append(motion_sum)
-    return motion_data
+
+    # check if there was motion in the last minute
+    t = now - timedelta(minutes=1).total_seconds()
+    with conn:
+        res = conn.execute('SELECT SUM(motion) FROM motion_logs WHERE time >= ?', [t])
+    thresh = 1
+    recent_motion = res.fetchone()[0]
+    if recent_motion is None:
+        recent_motion = False
+    else:
+        recent_motion = recent_motion > thresh
+    return motion_data, recent_motion
 
 if __name__ == '__main__':
     generate_test_data()
-    print( generate_sums(num_chunks=24, conn=sqlite3.connect('test.sqlite3')) )
+    print( get_motion_data( num_chunks=24, conn=sqlite3.connect('test.sqlite3') ) )
